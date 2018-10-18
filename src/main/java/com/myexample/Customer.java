6cbdba1cb70b0d58ebae67ea17b55a0349672d63
dmAdmin <>
2018-10-18 11:49:13 -0400

    {/src/main/java/com/myexample/Customer.java}

--- src/main/java/com/myexample/Customer.java
+++ src/main/java/com/myexample/Customer.java
@@ -1,17 +1,20 @@
 package com.myexample;
 
-import com.myexample.*;
+import com.myexample;
 import java.lang.String;
+import java.lang.StringBuilder;
 import java.io.Serializable;
 
 public class Customer implements Serializable {
 
 	static final long serialVersionUID = 1L;
 
-	@org.kie.api.definition.type.Label(value = "coupon")
+	@org.kie.api.definition.type.Label("coupon")
 	private String coupon;
-	@org.kie.api.definition.type.Label(value = "new")
+	@org.kie.api.definition.type.Label("new")
 	private boolean newCustomer;
+	@org.kie.api.definition.type.Label(value = "cart")
+	private Cart cart;
 
 	public Customer() {
 	}
@@ -32,9 +35,34 @@ public class Customer implements Serializable {
 		this.newCustomer = newCustomer;
 	}
 
-	public Customer(String coupon, boolean newCustomer) {
+	public Cart getCart() {
+		return this.cart;
+	}
+
+	public void setCart(Cart cart) {
+		this.cart = cart;
+	}
+	
+	public void addItem(Product product, int qty) {
+	    if (cart == null) {
+	        cart = new Cart(this);
+	    }
+	    cart.addItem(product, qty);
+	}
+	
+	public String toString() {
+	    StringBuilder sb = new StringBuilder();
+	    sb.append("New customer? ").append(isNewCustomer())
+	      .append("\nCoupon: ").append(coupon)
+	      .append("\n").append(cart);
+	    return sb.toString();
+	}
+
+	public Customer(java.lang.String coupon, boolean newCustomer,
+			com.myexample.Cart cart) {
 		this.coupon = coupon;
 		this.newCustomer = newCustomer;
+		this.cart = cart;
 	}
 
 }
